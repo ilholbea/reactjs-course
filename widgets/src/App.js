3cee9642d43bd7abad4e147dcab5d2d063cdefb4
Ionut-Laurentiu Holbea <ilholbea@gmail.com>
2021-03-14 23:29:12 +0100

    search widget

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import Accordion from './components/Accordion';
+import Search from './components/Search';
 
-const items = [
-  {
-    title: 'What is React?',
-    content: 'Frontend javascript framework',
-  },
-  {
-    title: 'Why use React?',
-    content: 'Great success!',
-  },
-  {
-    title: 'How to use React?',
-    content: 'Drugs',
-  },
-];
+// const items = [
+//   {
+//     title: 'What is React?',
+//     content: 'Frontend javascript framework',
+//   },
+//   {
+//     title: 'Why use React?',
+//     content: 'Great success!',
+//   },
+//   {
+//     title: 'How to use React?',
+//     content: 'Drugs',
+//   },
+// ];
 
 const App = () => {
   return (
     <div>
-      <br />
-      <Accordion items={items} />
+      <Search />
     </div>
   );
 };
