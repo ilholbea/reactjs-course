7a0aecaeeefef2be151802948f782e6978ae4365
Ionut-Laurentiu Holbea <ilholbea@gmail.com>
2021-03-16 23:40:06 +0100

    Videos with hooks

---
+++ videos_hooks/src/apis/youtube.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+//Get API Key from Youtube
+const KEY = '';
+
+export default axios.create({
+  baseURL: 'https://www.googleapis.com/youtube/v3',
+  params: {
+    part: 'snippet',
+    type: 'video',
+    maxResults: 5,
+    key: KEY,
+  },
+});
