2ee9974c302bac402d64d61d8d28f8906b5e54ae
Ionut-Laurentiu Holbea <ilholbea@gmail.com>
2021-04-11 20:56:09 +0200

    Final commit

--- blog/src/index.js
+++ blog/src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 
 import App from './components/App';
 import reducers from './reducers';
-import thunk from 'redux-thunk';
 
 const store = createStore(reducers, applyMiddleware(thunk));
+
 ReactDOM.render(
   <Provider store={store}>
     <App />
