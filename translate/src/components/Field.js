474033a2cc78daf406b3a40d9577ac69eb1eac66
Ionut-Laurentiu Holbea <ilholbea@gmail.com>
2021-05-14 15:20:09 +0200

    redux vs context example

--- translate/src/components/Field.js
+++ translate/src/components/Field.js
@@ -4,7 +4,7 @@ import LanguageContext from '../contexts/LanguageContext';
 class Field extends React.Component {
   static contextType = LanguageContext;
   render() {
-    const label = this.context === 'english' ? 'Name' : 'Naam';
+    const label = this.context.language === 'english' ? 'Name' : 'Naam';
     return (
       <div className="ui field">
         <label>{label}</label>
