db7201848dede4ead2a20554ae524f565232362e
Ionut-Laurentiu Holbea <ilholbea@gmail.com>
2021-05-14 14:20:30 +0200

    Implemented with context

---
+++ translate/src/components/Field.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import LanguageContext from '../contexts/LanguageContext';
+
+class Field extends React.Component {
+  static contextType = LanguageContext;
+  render() {
+    const label = this.context === 'english' ? 'Name' : 'Naam';
+    return (
+      <div className="ui field">
+        <label>{label}</label>
+        <input type="text" />
+      </div>
+    );
+  }
+}
+
+export default Field;
